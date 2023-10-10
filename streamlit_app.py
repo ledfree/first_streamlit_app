304989eb6a8f1bb58738e7da24de31d7451838ec
ledfree <ledfree@gmail.com>
2023-10-10 10:31:40 -0700

    Update streamlit_app.py
    add breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parennts New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.write('Omega 3 & Blueberry Oatmeal')
+streamlit.write('Kale, Spinach & Rocket Smoothie')
+streamlit.write('Hard-Boiled Free-Range Egg')
